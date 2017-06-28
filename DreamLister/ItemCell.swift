c5a5a9152df7285aa64f2d558e8ee91edbca5111
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-06-27 21:22:07 -0500

    connected the outlets from the ui view. Then get to core data.

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Ricardo Herrera Petit on 6/27/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
